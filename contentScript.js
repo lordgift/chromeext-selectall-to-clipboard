c8d6f2059c8f9771b5c258ebd8ed8e2720241c38
LordGift <jarupath.j@gmail.com>
2026-01-11 21:56:34 +0700

    more time to wait for UI update

--- contentScript.js
+++ contentScript.js
@@ -164,7 +164,7 @@ async function openMaskingEyesInTikTokOrderDetail() {
   });
 
   // Wait for the UI to update with unmasked data
-  await new Promise(resolve => setTimeout(resolve, 500));
+  await new Promise(resolve => setTimeout(resolve, 1000));
 }
 
 function publishMessageToOpenTab(tabJson) {
